e09a2384d0704257d1b256df34e993f570ebee94
Nugie kurniawan <nugiebinhar@gmail.com>
2025-09-22 21:34:55 +0700

    mencoba npm dan menjalankan nya

--- cobaNPM/app.js
+++ cobaNPM/app.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+// mengimport module thrid party
+// untuk memvalidasi data
+import validator from "validator"
+// untuk mengubah berwarna
+import chalk from "chalk"
+
+// mencoba valdator
+console.log(validator.isEmail("Nugie"))
+console.log(validator.isEmail("nugiekurniawan02@gmail.com"))
+console.log(validator.contains("javascript","javascript"))
+console.log(validator.isMobilePhone("087157933682","id-ID"))
+console.log(validator.isDecimal("087157933682"))
+// mencoba chalk
+console.log(chalk.blue.bgRed("Persib bandung"))
+console.log(chalk.bold.black.bgWhite("Nugie ganteng"))
